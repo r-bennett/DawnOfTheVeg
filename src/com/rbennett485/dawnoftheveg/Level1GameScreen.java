eb89fc9ec459b88c7d3661b114960b0dcbfb6dba
bennett_richard <rbennett485@gmail.com>
2014-07-21 18:02:26 +0100

    First game screen written (no abstract GameScreen superclass yet)

---
+++ src/com/rbennett485/dawnoftheveg/Level1GameScreen.java
@@ -0,0 +1,80 @@
+package com.rbennett485.dawnoftheveg;
+
+import java.util.List;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import com.badlogic.androidgames.framework.Game;
+import com.badlogic.androidgames.framework.Input.TouchEvent;
+import com.badlogic.androidgames.framework.gl.Camera2D;
+import com.badlogic.androidgames.framework.gl.SpriteBatcher;
+import com.badlogic.androidgames.framework.impl.GLScreen;
+import com.badlogic.androidgames.framework.math.OverlapTester;
+import com.badlogic.androidgames.framework.math.Rectangle;
+import com.badlogic.androidgames.framework.math.Vector2;
+
+public class Level1GameScreen extends GLScreen {
+
+	Camera2D guiCam;
+	SpriteBatcher batcher;
+	Rectangle backBounds;
+	Vector2 touchPoint;
+
+	public Level1GameScreen(Game game) {
+		super(game);
+		guiCam = new Camera2D(glGraphics, 800, 480);
+		batcher = new SpriteBatcher(glGraphics, 100);
+		backBounds = new Rectangle(760, 440, 40, 40);
+		touchPoint = new Vector2();
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
+		game.getInput().getKeyEvents();
+		int len = touchEvents.size();
+		for(int i=0 ; i<len ; i++) {
+			TouchEvent event = touchEvents.get(i);
+			touchPoint.set(event.x, event.y);
+			guiCam.touchToWorld(touchPoint);
+
+			if(event.type == TouchEvent.TOUCH_UP) {
+				if(OverlapTester.pointInRectangle(backBounds, touchPoint)) {
+					Assets.playSound(Assets.clickSound);
+					game.setScreen(new TitleScreen(game));
+					return;
+				}
+			}
+		}
+	}
+	
+    @Override
+    public void present(float deltaTime) {
+        GL10 gl = glGraphics.getGL();        
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        guiCam.setViewportAndMatrices();
+       
+        gl.glEnable(GL10.GL_BLEND);
+        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+        
+        batcher.beginBatch(Assets.icons);          
+        batcher.drawSprite(780, 460, 40, 40, Assets.back);
+        batcher.endBatch();
+        
+        gl.glDisable(GL10.GL_BLEND);
+    }
+    
+	@Override
+	public void pause() {
+		Settings.save(game.getFileIO());
+	}
+	
+	@Override
+	public void resume() {
+	}
+	
+	@Override 
+	public void dispose() {
+	}
+
+}
