ccea0712795cfb30ff04a73d8c56e0c387f106ce
bennett_richard <rbennett485@gmail.com>
2014-07-27 14:11:55 +0100

    updated listener

--- src/com/rbennett485/dawnoftheveg/World.java
+++ src/com/rbennett485/dawnoftheveg/World.java
@@ -9,13 +9,8 @@ import com.badlogic.androidgames.framework.GameObject;
 public class World {
 
 	public interface WorldListener {
-		public void jump();
-
-		public void highJump();
-
-		public void hit();
-
-		public void coin();
+		public void shot();
+		public void splat();
 	}
 
 	public static final float WORLD_WIDTH = 20;
@@ -24,42 +19,42 @@ public class World {
 	public static final int WORLD_STATE_RUNNING = 0;
 	public static final int WORLD_STATE_COMPLETE = 1;
 	public static final int WORLD_STATE_GAME_OVER = 2;
-	
+
 	public final List<GameObject> enemies;
 	public final List<GameObject> towers;
 	public final WorldListener listener;
 	public final Random rand;
-	
+
 	public int money;
-    public int state;
-    
-    public World(WorldListener listener) {
-        this.enemies = new ArrayList<GameObject>();
-        this.towers = new ArrayList<GameObject>();
-        this.listener = listener;
-        rand = new Random();
+	public int state;
 
-        this.money = INITIAL_MONEY;
-        this.state = WORLD_STATE_RUNNING;
-    }
+	public World(WorldListener listener) {
+		this.enemies = new ArrayList<GameObject>();
+		this.towers = new ArrayList<GameObject>();
+		this.listener = listener;
+		rand = new Random();
 
-    public void update(float deltaTime) {
-        updateTowers(deltaTime);
-        updateEnemies(deltaTime);
-        checkGameOver();
-    }
+		this.money = INITIAL_MONEY;
+		this.state = WORLD_STATE_RUNNING;
+	}
+
+	public void update(float deltaTime) {
+		updateTowers(deltaTime);
+		updateEnemies(deltaTime);
+		checkGameOver();
+	}
 
 	private void checkGameOver() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void updateEnemies(float deltaTime) {
 		int len = enemies.size();
-	    for (int i = 0; i < len; i++) {
-	        Orange orange = (Orange)enemies.get(i);  // Will need to change this once more types of enemy are added
-	        orange.update(deltaTime);
-	    }		
+		for (int i = 0; i < len; i++) {
+			Orange orange = (Orange)enemies.get(i);  // Will need to change this once more types of enemy are added
+			orange.update(deltaTime);
+		}		
 	}
 
 	private void updateTowers(float deltaTime) {
