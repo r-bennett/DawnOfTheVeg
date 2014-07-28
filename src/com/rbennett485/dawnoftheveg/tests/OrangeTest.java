112b14d4f872721d13b0f5663253e42b622f0f58
bennett_richard <rbennett485@gmail.com>
2014-07-28 17:43:06 +0100

    tests pass

--- src/com/rbennett485/dawnoftheveg/tests/OrangeTest.java
+++ src/com/rbennett485/dawnoftheveg/tests/OrangeTest.java
@@ -2,6 +2,29 @@ package com.rbennett485.dawnoftheveg.tests;
 
 import junit.framework.TestCase;
 
-public class OrangeTest extends TestCase {
+import org.junit.Before;
+import org.junit.Test;
 
+import com.rbennett485.dawnoftheveg.Orange;
+
+public class OrangeTest extends TestCase {
+	public Orange orange;
+	
+	@Before
+	public void setUp() {
+		orange = new Orange(2,4);
+	}
+	
+	@Test
+	public void testCreate() {
+		assertEquals(2,orange.position.x,0.00001);
+		assertEquals(4,orange.position.y,0.00001);
+	}
+	
+	@Test
+	public void testUpdate() {
+		orange.update(1);
+		assertEquals(2+orange.velocity.x*1,orange.position.x,0.00001);
+		assertEquals(4+orange.velocity.y*1,orange.position.y,0.00001);
+	}
 }
