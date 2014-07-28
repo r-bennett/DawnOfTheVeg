ebc2e2418504072d9e69eb8e1b07e22a1e254428
bennett_richard <rbennett485@gmail.com>
2014-07-28 18:26:35 +0100

    Initial commit

---
+++ src/com/rbennett485/dawnoftheveg/tests/WorldTest.java
@@ -0,0 +1,14 @@
+package com.rbennett485.dawnoftheveg.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
