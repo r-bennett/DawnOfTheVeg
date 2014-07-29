0ffe243cb33219e224e548bf831bd4c755389f2a
bennett_richard <rbennett485@gmail.com>
2014-07-29 16:34:34 +0100

    removed final modifier from velocity

--- src/com/badlogic/androidgames/framework/DynamicGameObject.java
+++ src/com/badlogic/androidgames/framework/DynamicGameObject.java
@@ -3,7 +3,7 @@ package com.badlogic.androidgames.framework;
 import com.badlogic.androidgames.framework.math.Vector2;
 
 public class DynamicGameObject extends GameObject {
-    public final Vector2 velocity;
+    public Vector2 velocity;
     public final Vector2 accel;
     
     public DynamicGameObject(float x, float y, float width, float height) {
