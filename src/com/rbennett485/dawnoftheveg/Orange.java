bbc0c4cf3b0a107f8f0a99b17226c3f5ff00d308
bennett_richard <rbennett485@gmail.com>
2014-07-29 13:32:53 +0100

    clone() reinstated

--- src/com/rbennett485/dawnoftheveg/Orange.java
+++ src/com/rbennett485/dawnoftheveg/Orange.java
@@ -16,4 +16,8 @@ public class Orange extends DynamicGameObject {
         bounds.lowerLeft.set(position).sub(ORANGE_WIDTH / 2, ORANGE_HEIGHT / 2);
 	}
 	
+	public Orange clone() {
+		return new Orange(this.position.x, this.position.y);
+	}
+	
 }
