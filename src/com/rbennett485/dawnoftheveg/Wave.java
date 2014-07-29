909ef69e1820dbf527cefe5ab2557cbc9dd50b3f
bennett_richard <rbennett485@gmail.com>
2014-07-29 11:21:07 +0100

    fields and constructor added. can only handle waves of a single enemy type for now

--- src/com/rbennett485/dawnoftheveg/Wave.java
+++ src/com/rbennett485/dawnoftheveg/Wave.java
@@ -1,5 +1,19 @@
 package com.rbennett485.dawnoftheveg;
 
-public class Wave {
+import java.util.List;
 
+import com.badlogic.androidgames.framework.DynamicGameObject;
+
+public class Wave {
+	public int time;
+	public DynamicGameObject seed;
+	public int number;
+	
+	public Wave(int time, DynamicGameObject seed, int number) {
+		this.time = time;
+		this.seed = seed;
+		this.number = number;
+	}
+	
+	
 }
