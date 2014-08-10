a2f5b732045517899ffabccd56bf88f27d44ccd2
bennett_richard <rbennett485@gmail.com>
2014-08-10 22:20:10 +0100

    added tower patches

--- src/com/rbennett485/dawnoftheveg/Level.java
+++ src/com/rbennett485/dawnoftheveg/Level.java
@@ -8,14 +8,16 @@ public abstract class Level {
 	public final int INITIAL_MONEY;
 	public final List<Vector2> wayPoints;
 	public final List<Wave> waves;
+	public final List<Vector2> towerPatches;
 	
 	public Level(int INITIAL_MONEY) {
 		this.INITIAL_MONEY = INITIAL_MONEY;
 		this.wayPoints = wayPointGenerator();
 		this.waves = waveGenerator();
+		this.towerPatches = towerPatchGenerator();
 	}
 
 	public abstract List<Vector2> wayPointGenerator();
 	public abstract List<Wave> waveGenerator();
-
+	public abstract List<Vector2> towerPatchGenerator();
 }
