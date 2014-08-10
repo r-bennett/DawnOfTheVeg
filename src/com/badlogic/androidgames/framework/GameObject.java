c98600e7fe9872c99522ac56fba197e4ccec223d
bennett_richard <rbennett485@gmail.com>
2014-08-10 13:21:36 +0100

    added boolean inGame

--- src/com/badlogic/androidgames/framework/GameObject.java
+++ src/com/badlogic/androidgames/framework/GameObject.java
@@ -6,8 +6,10 @@ import com.badlogic.androidgames.framework.math.Vector2;
 public class GameObject {
     public final Vector2 position;
     public final Rectangle bounds;
+	public boolean inGame;
     
     public GameObject(float x, float y, float width, float height) {
+    	inGame = true;
         this.position = new Vector2(x,y);
         this.bounds = new Rectangle(x-width/2, y-height/2, width, height);
     }
@@ -16,5 +18,9 @@ public class GameObject {
     public GameObject clone() {
     	return new GameObject(this.position.x, this.position.y, 
     			this.bounds.width, this.bounds.height); 
+    }
+    
+    public void update(float deltaTime) {
+ 
     }
 }
