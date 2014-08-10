89b27516ee61b42598163cfa136eef4b15da2575
bennett_richard <rbennett485@gmail.com>
2014-08-10 12:46:41 +0100

    updated waypoints and waves

--- src/com/rbennett485/dawnoftheveg/Level1.java
+++ src/com/rbennett485/dawnoftheveg/Level1.java
@@ -14,7 +14,8 @@ class Level1 extends Level {
 	public List<Vector2> wayPointGenerator() {
 		List<Vector2> result = new ArrayList<>();
 		result.add(new Vector2(0,6));
-		result.add(new Vector2(10,0));
+		result.add(new Vector2(10,3));
+		result.add(new Vector2(15,11));
 		result.add(new Vector2(20,6));
 		return result;
 	}
@@ -22,7 +23,8 @@ class Level1 extends Level {
 	@Override
 	public List<Wave> waveGenerator() {
 		List<Wave> result = new ArrayList<>();
-		result.add(new Wave(0, new Orange(wayPoints), 1));
+		result.add(new Wave(0, new Orange(wayPoints), 3));
+		result.add(new Wave(10, new Orange(wayPoints), 5));
 		return result;
 	}
 	
