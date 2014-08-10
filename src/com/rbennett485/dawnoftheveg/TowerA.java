d6b949503efa61c43354113c76eea95fbeb9812b
bennett_richard <rbennett485@gmail.com>
2014-08-10 15:42:59 +0100

    Initial commit

---
+++ src/com/rbennett485/dawnoftheveg/TowerA.java
@@ -0,0 +1,11 @@
+package com.rbennett485.dawnoftheveg;
+
+import com.badlogic.androidgames.framework.GameObject;
+
+public class TowerA extends GameObject {
+
+	public TowerA(float x, float y, float width, float height) {
+		super(x, y, width, height);
+	}
+
+}
