3083b24ac39ecdcfdb1c3b9e22392c6cba40099c
bennett_richard <rbennett485@gmail.com>
2014-08-11 13:09:24 +0100

    added towerMenu texture region

--- src/com/rbennett485/dawnoftheveg/Assets.java
+++ src/com/rbennett485/dawnoftheveg/Assets.java
@@ -14,6 +14,7 @@ public class Assets {
 	public static TextureRegion towerB;
 	public static TextureRegion towerC;
 	public static TextureRegion towerD;
+	public static TextureRegion towerMenu;
 	
 	public static Texture healthBars;
 	public static TextureRegion healthBarRed;
@@ -53,6 +54,7 @@ public class Assets {
 		towerB = new TextureRegion(towerImage, 80, 0, 40, 40);
 		towerC = new TextureRegion(towerImage, 0, 40, 40, 40);
 		towerD = new TextureRegion(towerImage, 40, 40, 40, 40);
+		towerMenu = new TextureRegion(towerImage, 120, 0, 80, 80);
 		
 		healthBars = new Texture(game, "healthBars.png");
 		healthBarRed = new TextureRegion(healthBars, 0, 0, 32, 3);
