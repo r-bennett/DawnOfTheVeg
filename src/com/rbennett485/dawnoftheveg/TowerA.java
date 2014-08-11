1906057a32fdf50727921adc8d2c23016da33e2d
bennett_richard <rbennett485@gmail.com>
2014-08-11 23:15:17 +0100

    reload time added

--- src/com/rbennett485/dawnoftheveg/TowerA.java
+++ src/com/rbennett485/dawnoftheveg/TowerA.java
@@ -3,6 +3,7 @@ package com.rbennett485.dawnoftheveg;
 import com.badlogic.androidgames.framework.GameObject;
 
 public class TowerA extends Tower {
+	public static final float RELOAD_TIME = 0.5f;
 	
 	public TowerA(float x, float y, float width, float height) {
 		super(x, y, width, height);
