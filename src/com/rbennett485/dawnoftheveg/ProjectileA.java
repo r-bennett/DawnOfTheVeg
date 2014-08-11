0faf4e23e3257f7c827fd9d99e09fc7eb7e1c074
bennett_richard <rbennett485@gmail.com>
2014-08-11 23:04:52 +0100

    initial commit

---
+++ src/com/rbennett485/dawnoftheveg/ProjectileA.java
@@ -0,0 +1,10 @@
+package com.rbennett485.dawnoftheveg;
+
+public class ProjectileA extends Projectile {
+
+	public ProjectileA(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
