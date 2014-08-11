7fad8bd5254824d2b27074973fa754439b1df749
bennett_richard <rbennett485@gmail.com>
2014-08-11 23:03:36 +0100

    Parent class for tower projectiles

---
+++ src/com/rbennett485/dawnoftheveg/Projectile.java
@@ -0,0 +1,12 @@
+package com.rbennett485.dawnoftheveg;
+
+import com.badlogic.androidgames.framework.DynamicGameObject;
+
+public class Projectile extends DynamicGameObject {
+
+	public Projectile(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
