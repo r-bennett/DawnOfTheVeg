d4f6842f4f54e1e4a54bbe49a36a0ede097efee7
bennett_richard <rbennett485@gmail.com>
2014-08-11 23:11:10 +0100

    made abstract

--- src/com/rbennett485/dawnoftheveg/Tower.java
+++ src/com/rbennett485/dawnoftheveg/Tower.java
@@ -2,11 +2,10 @@ package com.rbennett485.dawnoftheveg;
 
 import com.badlogic.androidgames.framework.GameObject;
 
-public class Tower extends GameObject {
+public abstract class Tower extends GameObject {
 
 	public Tower(float x, float y, float width, float height) {
 		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
 	}
 
 }
