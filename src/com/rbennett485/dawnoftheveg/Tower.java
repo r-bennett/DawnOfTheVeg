cf7c4c45fa05c06ecfcf97f243be6dafa5167fce
bennett_richard <rbennett485@gmail.com>
2014-08-11 23:02:40 +0100

    Parent class for Towers,intial commit

---
+++ src/com/rbennett485/dawnoftheveg/Tower.java
@@ -0,0 +1,12 @@
+package com.rbennett485.dawnoftheveg;
+
+import com.badlogic.androidgames.framework.GameObject;
+
+public class Tower extends GameObject {
+
+	public Tower(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
