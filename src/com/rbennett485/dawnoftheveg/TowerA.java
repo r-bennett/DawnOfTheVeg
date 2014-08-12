448a163d53a26beac904314f01869e6ab4fdb475
bennett_richard <rbennett485@gmail.com>
2014-08-12 12:41:47 +0100

    ??

--- src/com/rbennett485/dawnoftheveg/TowerA.java
+++ src/com/rbennett485/dawnoftheveg/TowerA.java
@@ -6,11 +6,11 @@ public class TowerA extends Tower {
 	public static final float TOWER_A_WIDTH = 1f;
 	public static final float TOWER_A_HEIGHT = 1f;
 	public static final int TOWER_A_COST = 100;
-	
+
 	public TowerA(float x, float y) {
 		this(x, y, TOWER_A_WIDTH, TOWER_A_HEIGHT);
 	}
-	
+
 	private TowerA(float x, float y, float width, float height) {
 		super(x, y, width, height);
 	}
