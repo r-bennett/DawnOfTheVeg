7d980f593207f20eee18905e457ae3ebf71534f9
bennett_richard <rbennett485@gmail.com>
2014-08-12 16:33:59 +0100

    damagePoints set

--- src/com/rbennett485/dawnoftheveg/ProjectileA.java
+++ src/com/rbennett485/dawnoftheveg/ProjectileA.java
@@ -1,10 +1,11 @@
 package com.rbennett485.dawnoftheveg;
 
 public class ProjectileA extends Projectile {
-
+	public static final int PROJECTILE_A_DAMAGE = 4;
+	
 	public ProjectileA(float x, float y, float width, float height) {
 		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
+		damagePoints = PROJECTILE_A_DAMAGE;
 	}
 
 }
