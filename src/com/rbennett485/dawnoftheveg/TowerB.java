54e91f59f7c1359da656bda0f229f3e36c871f0a
bennett_richard <rbennett485@gmail.com>
2014-08-12 10:59:44 +0100

    amended constructors and fields

--- src/com/rbennett485/dawnoftheveg/TowerB.java
+++ src/com/rbennett485/dawnoftheveg/TowerB.java
@@ -2,10 +2,16 @@ package com.rbennett485.dawnoftheveg;
 
 public class TowerB extends Tower {
 	public static final float RELOAD_TIME = 0.5f;
-
-	public TowerB(float x, float y, float width, float height) {
-		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
+	public static final float TOWER_B_WIDTH = 1f;
+	public static final float TOWER_B_HEIGHT = 1f;
+	
+	public TowerB(float x, float y) {
+		this(x, y, TOWER_B_WIDTH, TOWER_B_HEIGHT);
 	}
+	
+	private TowerB(float x, float y, float width, float height) {
+		super(x, y, width, height);
+	}
+
 
 }
--- src/com/rbennett485/dawnoftheveg/TowerC.java
+++ src/com/rbennett485/dawnoftheveg/TowerC.java
@@ -2,10 +2,15 @@ package com.rbennett485.dawnoftheveg;
 
 public class TowerC extends Tower {
 	public static final float RELOAD_TIME = 0.5f;
-
-	public TowerC(float x, float y, float width, float height) {
+	public static final float TOWER_C_WIDTH = 1f;
+	public static final float TOWER_C_HEIGHT = 1f;
+	
+	public TowerC(float x, float y) {
+		this(x, y, TOWER_C_WIDTH, TOWER_C_HEIGHT);
+	}
+	
+	private TowerC(float x, float y, float width, float height) {
 		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
 	}
 
 }
