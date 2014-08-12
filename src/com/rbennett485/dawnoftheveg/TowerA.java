56a1d4fa84b5d33ccafe9fe5ea8921d3d73bec15
bennett_richard <rbennett485@gmail.com>
2014-08-12 10:54:09 +0100

    changed constructor to automatically add height and width

--- src/com/rbennett485/dawnoftheveg/TowerA.java
+++ src/com/rbennett485/dawnoftheveg/TowerA.java
@@ -3,8 +3,14 @@ package com.rbennett485.dawnoftheveg;
 
 public class TowerA extends Tower {
 	public static final float RELOAD_TIME = 0.5f;
+	public static final float TOWER_A_WIDTH = 1f;
+	public static final float TOWER_A_HEIGHT = 1f;
 	
-	public TowerA(float x, float y, float width, float height) {
+	public TowerA(float x, float y) {
+		this(x, y, TOWER_A_WIDTH, TOWER_A_HEIGHT);
+	}
+	
+	private TowerA(float x, float y, float width, float height) {
 		super(x, y, width, height);
 	}
 
