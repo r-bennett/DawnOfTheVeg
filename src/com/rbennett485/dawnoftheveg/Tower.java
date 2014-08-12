cbacd2c2647072c0ff798c9bfc46d8c4aa82ad81
bennett_richard <rbennett485@gmail.com>
2014-08-12 15:46:36 +0100

    reloadTime added

--- src/com/rbennett485/dawnoftheveg/Tower.java
+++ src/com/rbennett485/dawnoftheveg/Tower.java
@@ -1,11 +1,18 @@
 package com.rbennett485.dawnoftheveg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.androidgames.framework.GameObject;
 
 public abstract class Tower extends GameObject {
+	public float idleTime;
+	public float reloadTime;
+	public List<Projectile> projectiles;
 
 	public Tower(float x, float y, float width, float height) {
 		super(x, y, width, height);
+		projectiles = new ArrayList<>();
 	}
 	
 	abstract int getCost();
