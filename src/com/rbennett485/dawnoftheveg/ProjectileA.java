279f1c066808b2741dbf52a2780c0f8a72b15cae
bennett_richard <rbennett485@gmail.com>
2014-08-12 16:44:25 +0100

    velocity added

--- src/com/rbennett485/dawnoftheveg/ProjectileA.java
+++ src/com/rbennett485/dawnoftheveg/ProjectileA.java
@@ -2,10 +2,12 @@ package com.rbennett485.dawnoftheveg;
 
 public class ProjectileA extends Projectile {
 	public static final int PROJECTILE_A_DAMAGE = 4;
+	public static final float PROJECTILE_A_VELOCITY = 5f;
 	
 	public ProjectileA(float x, float y, float width, float height) {
 		super(x, y, width, height);
 		damagePoints = PROJECTILE_A_DAMAGE;
+		velocity = PROJECTILE_A_VELOCITY;
 	}
 
 }
