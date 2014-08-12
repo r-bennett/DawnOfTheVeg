9b00fc83f30acbbb76e4ae9fb0193e61d97fbc5e
bennett_richard <rbennett485@gmail.com>
2014-08-12 10:13:06 +0100

    initial commit

---
+++ src/com/rbennett485/dawnoftheveg/TowerB.java
@@ -0,0 +1,10 @@
+package com.rbennett485.dawnoftheveg;
+
+public class TowerB extends Tower {
+
+	public TowerB(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/rbennett485/dawnoftheveg/TowerC.java
@@ -0,0 +1,10 @@
+package com.rbennett485.dawnoftheveg;
+
+public class TowerC extends Tower {
+
+	public TowerC(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
