13f10aa9f3e1f40810f5cedf8b2a0db63f5a7174
bennett_richard <rbennett485@gmail.com>
2014-08-12 16:57:26 +0100

    velocity changed to speed

--- src/com/rbennett485/dawnoftheveg/Projectile.java
+++ src/com/rbennett485/dawnoftheveg/Projectile.java
@@ -5,7 +5,7 @@ import com.badlogic.androidgames.framework.DynamicGameObject;
 public class Projectile extends DynamicGameObject {
 
 	public int damagePoints;
-	public float velocity;
+	public float speed;
 
 	public Projectile(float x, float y, float width, float height) {
 		super(x, y, width, height);
--- src/com/rbennett485/dawnoftheveg/ProjectileA.java
+++ src/com/rbennett485/dawnoftheveg/ProjectileA.java
@@ -1,13 +1,22 @@
 package com.rbennett485.dawnoftheveg;
 
+import com.badlogic.androidgames.framework.math.Vector2;
+
 public class ProjectileA extends Projectile {
 	public static final int PROJECTILE_A_DAMAGE = 4;
-	public static final float PROJECTILE_A_VELOCITY = 5f;
+	public static final float PROJECTILE_A_SPEED = 5f;
+	public static final float PROJECTILE_A_WIDTH = 0.2f;
+	public static final float PROJECTILE_A_HEIGHT = 0.2f;
 	
-	public ProjectileA(float x, float y, float width, float height) {
+	public ProjectileA(Vector2 position, Vector2 velocity) {
+		this(position.x, position.y, PROJECTILE_A_WIDTH, PROJECTILE_A_HEIGHT);
+		this.velocity = velocity;
+	}
+	
+	private ProjectileA(float x, float y, float width, float height) {
 		super(x, y, width, height);
 		damagePoints = PROJECTILE_A_DAMAGE;
-		velocity = PROJECTILE_A_VELOCITY;
+		speed = PROJECTILE_A_SPEED;
 	}
 
 }
