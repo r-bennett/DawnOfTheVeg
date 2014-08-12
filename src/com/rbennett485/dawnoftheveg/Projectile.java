1561c2d078a4f465b5f91adbe3857dc77c334eaf
bennett_richard <rbennett485@gmail.com>
2014-08-12 16:33:50 +0100

    damagePoints field added

--- src/com/rbennett485/dawnoftheveg/Projectile.java
+++ src/com/rbennett485/dawnoftheveg/Projectile.java
@@ -4,6 +4,8 @@ import com.badlogic.androidgames.framework.DynamicGameObject;
 
 public class Projectile extends DynamicGameObject {
 
+	public int damagePoints;
+
 	public Projectile(float x, float y, float width, float height) {
 		super(x, y, width, height);
 		// TODO Auto-generated constructor stub
