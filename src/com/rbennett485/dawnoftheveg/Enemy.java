596195574799e8a795475e6e1d8bbc9389019b44
bennett_richard <rbennett485@gmail.com>
2014-08-12 15:32:14 +0100

    initial commit

---
+++ src/com/rbennett485/dawnoftheveg/Enemy.java
@@ -0,0 +1,12 @@
+package com.rbennett485.dawnoftheveg;
+
+import com.badlogic.androidgames.framework.DynamicGameObject;
+
+public class Enemy extends DynamicGameObject {
+
+	public Enemy(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
