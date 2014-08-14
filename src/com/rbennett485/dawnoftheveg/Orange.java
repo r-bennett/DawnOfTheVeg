9cdf13fe570506d310787e7afed191eedb740db1
bennett_richard <rbennett485@gmail.com>
2014-08-14 10:15:18 +0100

    removed logging

--- src/com/rbennett485/dawnoftheveg/Orange.java
+++ src/com/rbennett485/dawnoftheveg/Orange.java
@@ -2,8 +2,6 @@ package com.rbennett485.dawnoftheveg;
 
 import java.util.List;
 
-import android.util.Log;
-
 import com.badlogic.androidgames.framework.math.Vector2;
 
 public class Orange extends Enemy {
@@ -13,7 +11,7 @@ public class Orange extends Enemy {
 	public static final int ORANGE_HP = 20;
 	public final List<Vector2> wayPoints;
 	public int nextWayPoint;
-	
+
 	public Orange(List<Vector2> wayPoints) {
 		this(wayPoints.get(0).x,wayPoints.get(0).y, wayPoints);
 	}
@@ -24,25 +22,17 @@ public class Orange extends Enemy {
 		initialHp = ORANGE_HP;
 		hp = initialHp;
 		nextWayPoint = 0; // create the orange at waypoint 0, have it head to waypoint 1
-		Log.d("coords", this.wayPoints.get(0).x + ", " + this.wayPoints.get(0).y + "\n" +
-				this.wayPoints.get(1).x + ", " + this.wayPoints.get(1).y + "\n");
 	}
 
 	public void update(float deltaTime) {
 		if(nextWayPoint<wayPoints.size()) {
 			if(position.dist(wayPoints.get(nextWayPoint))<0.1) {
-//				Log.d("wp", nextWayPoint +"\n" +
-//							"pos: " + this.position.x + ", " + this.position.y + "\n" +
-//							"previous wp: " + this.wayPoints.get(nextWayPoint).x + ", " + this.wayPoints.get(nextWayPoint).y + "\n");
 				nextWayPoint++;
-//				Log.d("wp", "first next wp: " + this.wayPoints.get(nextWayPoint).x + ", " + this.wayPoints.get(nextWayPoint).y);
 				if(nextWayPoint<wayPoints.size()) {
 					velocity.set(wayPoints.get(nextWayPoint).cpy().sub(this.position).nor().mul(ORANGE_VELOCITY));
-//					Log.d("wp", "vel: " + this.velocity.x  + ", " + this.velocity.y + "\n" +
-//							"next wp: " + this.wayPoints.get(nextWayPoint).x + ", " + this.wayPoints.get(nextWayPoint).y);
 				}
 			}
-			
+
 		} 
 
 		position.add(velocity.x * deltaTime, velocity.y * deltaTime);
