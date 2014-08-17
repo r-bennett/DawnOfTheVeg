a6a27bd9d5c934f5091ea2d6e17399dae10bfe71
bennett_richard <rbennett485@gmail.com>
2014-08-17 22:43:37 +0100

    initial commit - fails test

---
+++ src/com/rbennett485/dawnoftheveg/tests/ProjectileTest.java
@@ -0,0 +1,14 @@
+package com.rbennett485.dawnoftheveg.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProjectileTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
