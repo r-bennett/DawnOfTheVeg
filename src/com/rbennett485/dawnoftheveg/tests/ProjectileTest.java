5b54a8a03683e36d3d03ed04e86823ce5d79fd33
bennett_richard <rbennett485@gmail.com>
2014-08-17 23:07:16 +0100

    passes update test

--- src/com/rbennett485/dawnoftheveg/tests/ProjectileTest.java
+++ src/com/rbennett485/dawnoftheveg/tests/ProjectileTest.java
@@ -1,14 +1,46 @@
 package com.rbennett485.dawnoftheveg.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import com.badlogic.androidgames.framework.math.Vector2;
+import com.rbennett485.dawnoftheveg.Projectile;
+import com.rbennett485.dawnoftheveg.ProjectileA;
+import com.rbennett485.dawnoftheveg.ProjectileB;
+import com.rbennett485.dawnoftheveg.ProjectileC;
+import com.rbennett485.dawnoftheveg.ProjectileD;
+
 public class ProjectileTest {
+	
+	private Projectile projA;
+	private Projectile projB;
+	private Projectile projC;
+	private Projectile projD;
+	
+	@Before
+	public void buildUp() {
+		projA = new ProjectileA(new Vector2(0,0), new Vector2(2,1));
+		projB = new ProjectileB(new Vector2(0,0), new Vector2(2,1));
+		projC = new ProjectileC(new Vector2(0,0), new Vector2(2,1));
+		projD = new ProjectileD(new Vector2(0,0), new Vector2(2,1));
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUpdate() {
+		testUpdateHelper(projA);
+		testUpdateHelper(projB);
+		testUpdateHelper(projC);
+		testUpdateHelper(projD);
+	}
+	
+	public void  testUpdateHelper(Projectile proj) {
+		float originalX = proj.position.x;
+		float originalY = proj.position.y;
+		proj.update(1);
+		assertEquals(proj.position.x, (originalX + proj.velocity.x), 0.0001);
+		assertEquals(proj.position.y, (originalY + proj.velocity.y), 0.0001);
 	}
 
 }
