cd8a63ac01d5bfa715cd44b3d516d7e5deb26629
bennett_richard <rbennett485@gmail.com>
2014-08-17 17:20:46 +0100

    initial commit - fails

--- src/com/rbennett485/dawnoftheveg/tests/WorldTest.java
+++ src/com/rbennett485/dawnoftheveg/tests/WorldTest.java
@@ -1,11 +1,40 @@
 package com.rbennett485.dawnoftheveg.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class WorldTest {
+import com.rbennett485.dawnoftheveg.Level;
+import com.rbennett485.dawnoftheveg.Level1;
+import com.rbennett485.dawnoftheveg.World;
+import com.rbennett485.dawnoftheveg.World.WorldListener;
 
+public class WorldTest {
+	private World world;
+	private WorldListener worldListener;
+	private Level level;
+
+	
+	@Before
+	public void buildUp() {
+		level = new Level1();
+		worldListener = new WorldListener() {
+
+			@Override
+			public void shot() {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void splat() {
+				// TODO Auto-generated method stub
+			}
+
+		};
+		world = new World(worldListener, level);
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
