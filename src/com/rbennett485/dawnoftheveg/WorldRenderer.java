04a6b55e4bbd75777e5661596d0736425b7185d5
bennett_richard <rbennett485@gmail.com>
2014-08-24 00:07:40 +0100

    added conditional to tower rendering to stop empty batch

--- src/com/rbennett485/dawnoftheveg/WorldRenderer.java
+++ src/com/rbennett485/dawnoftheveg/WorldRenderer.java
@@ -44,10 +44,12 @@ public class WorldRenderer {
 		gl.glEnable(GL10.GL_BLEND);
 		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 
-		batcher.beginBatch(Assets.towerImage);
-		renderTowers();
-		renderProjectiles();
-		batcher.endBatch();
+		if(world.towers.size()!=0) {
+			batcher.beginBatch(Assets.towerImage);
+			renderTowers();
+			renderProjectiles();
+			batcher.endBatch();
+		}
 
 		if(!world.enemies.isEmpty()) {
 			batcher.beginBatch(Assets.characters);
