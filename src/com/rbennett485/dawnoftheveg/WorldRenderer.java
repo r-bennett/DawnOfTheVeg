2a3a2f46ff5b060eeae555e7da18219e9de621bd
bennett_richard <rbennett485@gmail.com>
2014-08-23 22:20:03 +0100

    renders the background from world's level

--- src/com/rbennett485/dawnoftheveg/WorldRenderer.java
+++ src/com/rbennett485/dawnoftheveg/WorldRenderer.java
@@ -33,10 +33,10 @@ public class WorldRenderer {
 	}
 
 	private void renderBackground() {
-		batcher.beginBatch(Assets.background);
+		batcher.beginBatch(Assets.level1background);
 		batcher.drawSprite(cam.position.x, cam.position.y,
 				FRUSTUM_WIDTH, FRUSTUM_HEIGHT, 
-				Assets.backgroundRegion);
+				world.level.background);
 		batcher.endBatch();
 	}
 
