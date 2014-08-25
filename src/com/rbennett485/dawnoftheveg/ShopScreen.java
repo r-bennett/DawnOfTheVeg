d00dd9925c858952a34d375b4ad365374cd704e5
bennett_richard <rbennett485@gmail.com>
2014-08-25 18:22:04 +0100

    updated icons to sprites

--- src/com/rbennett485/dawnoftheveg/ShopScreen.java
+++ src/com/rbennett485/dawnoftheveg/ShopScreen.java
@@ -82,15 +82,9 @@ public class ShopScreen extends GLScreen {
 		gl.glEnable(GL10.GL_BLEND);
 		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 
-		batcher.beginBatch(Assets.icons);
+		batcher.beginBatch(Assets.sprites);
 		presentIcons();
-		batcher.endBatch();
-
-		batcher.beginBatch(Assets.towerImage);
 		presentItem();
-		batcher.endBatch();
-
-		batcher.beginBatch(Assets.fontImage);
 		presentText();
 		batcher.endBatch();
 
