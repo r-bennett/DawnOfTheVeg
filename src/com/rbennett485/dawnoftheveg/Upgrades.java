886d08dea36eabd8ca01f1e11e158ce6cf1f7574
bennett_richard <rbennett485@gmail.com>
2014-08-27 17:43:40 +0100

    initial commit

---
+++ src/com/rbennett485/dawnoftheveg/Upgrades.java
@@ -0,0 +1,13 @@
+package com.rbennett485.dawnoftheveg;
+
+public class Upgrades {
+	
+	public static final int RANGE_COST = 40;
+	public static final int DAMAGE_COST = 60;
+	public static final int RELOAD_COST = 80;
+	
+	public static final float RANGE_FACTOR = 1.5f;
+	public static final float DAMAGE_FACTOR = 1.5f;
+	public static final float RELOAD_FACTOR = 0.5f;
+
+}
