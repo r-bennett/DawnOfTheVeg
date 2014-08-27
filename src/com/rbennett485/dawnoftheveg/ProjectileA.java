f277414b3ab1545cdcdb0b7fda5c0ef6d6c22d1d
bennett_richard <rbennett485@gmail.com>
2014-08-27 18:40:25 +0100

    added damage to constructors

--- src/com/rbennett485/dawnoftheveg/ProjectileA.java
+++ src/com/rbennett485/dawnoftheveg/ProjectileA.java
@@ -3,19 +3,18 @@ package com.rbennett485.dawnoftheveg;
 import com.badlogic.androidgames.framework.math.Vector2;
 
 public class ProjectileA extends Projectile {
-	public static final int PROJECTILE_A_DAMAGE = 4;
 	public static final float PROJECTILE_A_SPEED = 5f;
 	public static final float PROJECTILE_A_WIDTH = 0.2f;
 	public static final float PROJECTILE_A_HEIGHT = 0.2f;
 	
-	public ProjectileA(Vector2 position, Vector2 velocity) {
+	public ProjectileA(Vector2 position, Vector2 velocity, int damage) {
 		this(position.x, position.y, PROJECTILE_A_WIDTH, PROJECTILE_A_HEIGHT);
 		this.velocity = velocity;
+		this.damagePoints = damage;
 	}
 	
 	private ProjectileA(float x, float y, float width, float height) {
 		super(x, y, width, height);
-		damagePoints = PROJECTILE_A_DAMAGE;
 		speed = PROJECTILE_A_SPEED;
 	}
 
