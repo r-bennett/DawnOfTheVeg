d856bb891bf881d0c79c8ec68e7d04781e14b694
bennett_richard <rbennett485@gmail.com>
2014-08-27 15:28:40 +0100

    added all textures to reload()

--- src/com/rbennett485/dawnoftheveg/Assets.java
+++ src/com/rbennett485/dawnoftheveg/Assets.java
@@ -44,15 +44,15 @@ public class Assets {
 	public static TextureRegion towerMenu;
 	public static TextureRegion cross;
 	public static TextureRegion hazard;
-	
+
 	public static Font font;
-	
+
 	public static Texture background;
 	public static TextureRegion backgroundRegion;
-	
+
 	public static Texture map;
 	public static TextureRegion mapRegion;
-	
+
 	public static Texture level1background;
 	public static TextureRegion level1backgroundRegion;
 	public static Texture level2background;
@@ -73,17 +73,17 @@ public class Assets {
 	public static Texture helpImage;
 	public static TextureRegion[] helpRegion;
 	public static int NUMBER_OF_HELP_REGIONS = 2;
-	
+
 	public static Texture[] story;
 	public static TextureRegion[] storyRegion;
 	public static int NUMBER_OF_STORY_REGIONS = 12;
-	
+
 	public static Music music;
-	
+
 	public static Sound clickSound;
-	
+
 	public static void load(GLGame game) {
-		
+
 		sprites = new Texture(game, "spritesheet.png");
 		patch = new TextureRegion(sprites, 336, 0, 40, 40);
 		towerA = new TextureRegion(sprites, 871, 0, 50, 81);
@@ -121,13 +121,13 @@ public class Assets {
 		hazard = new TextureRegion(sprites, 881, 90, 45, 40);
 		// Remove this only temporary botch ********************************************************************************
 		towerMenu = new TextureRegion(sprites, 456, 30, 25, 23);
-		
-		
+
+
 		font = new Font(sprites, 1, 307, 16, 16, 32);
-		
+
 		map =  new Texture(game, "map.png");
 		mapRegion = new TextureRegion(map, 0, 0, 800, 480);
-				
+
 		level1background = new Texture(game, "level_1_background.png");
 		level1backgroundRegion = new TextureRegion(level1background, 0, 0, 800, 480);
 		level2background = new Texture(game, "level_2_background.png");
@@ -144,15 +144,15 @@ public class Assets {
 		level7backgroundRegion = new TextureRegion(level7background, 0, 0, 800, 480);
 		level8background = new Texture(game, "level_8_background.png");
 		level8backgroundRegion = new TextureRegion(level8background, 0, 0, 800, 480);
-		
+
 		background = new Texture(game,"background.png");
 		backgroundRegion = new TextureRegion(background, 0, 0, 4, 4);
-				
+
 		helpImage = new Texture(game, "help.png");
 		helpRegion =  new TextureRegion[NUMBER_OF_HELP_REGIONS];
 		helpRegion[0] =  new TextureRegion(helpImage, 0, 0, 560, 320);
 		helpRegion[1] = new TextureRegion(helpImage, 560, 0, 560, 320);
-		
+
 		story = new Texture[NUMBER_OF_STORY_REGIONS];
 		storyRegion = new TextureRegion[NUMBER_OF_STORY_REGIONS];
 		story[0] = new Texture(game, "story0.png");
@@ -179,28 +179,35 @@ public class Assets {
 		storyRegion[10] = new TextureRegion(story[10], 0, 0, 800, 480);
 		story[11] = new Texture(game, "story11.png");
 		storyRegion[11] = new TextureRegion(story[11], 0, 0, 800, 480);
-		
+
 		music = game.getAudio().newMusic("music.mp3");
 		music.setLooping(true);
 		music.setVolume(0.5f);
 		if(Settings.soundEnabled)
-				music.play();
+			music.play();
 		clickSound = game.getAudio().newSound("click.ogg");
 	}
-	
+
 	public static void reload() {
+		sprites.reload();
 		background.reload();
+		map.reload();
 		level1background.reload();
 		level2background.reload();
 		level3background.reload();
 		level4background.reload();
-		sprites.reload();
+		level5background.reload();
+		level6background.reload();
+		level7background.reload();
+		level8background.reload();
 		helpImage.reload();
+		for(Texture t : story) 
+			t.reload();
 		
 		if(Settings.soundEnabled)
 			music.play();
 	}
-	
+
 	public static void playSound(Sound sound) {
 		if(Settings.soundEnabled)
 			sound.play(1);
