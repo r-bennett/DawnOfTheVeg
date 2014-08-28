553eeaf5b1e27ea0f3455d71a8902b59c10ebb43
bennett_richard <rbennett485@gmail.com>
2014-08-28 12:02:03 +0100

    new pea wave

--- src/com/rbennett485/dawnoftheveg/Level1.java
+++ src/com/rbennett485/dawnoftheveg/Level1.java
@@ -28,7 +28,8 @@ public class Level1 extends Level {
 	@Override
 	public List<Wave> waveGenerator() {
 		List<Wave> result = new ArrayList<>();
-		result.add(new Wave(0, new Orange(wayPoints), 10));
+		result.add(new Wave(0, new Orange(wayPoints), 3));
+		result.add(new Wave(5, new Pea(wayPoints), 5));
 		result.add(new Wave(10, new Chilli(wayPoints), 8));
 		result.add(new Wave(20, new Grape(wayPoints), 3));
 		return result;
