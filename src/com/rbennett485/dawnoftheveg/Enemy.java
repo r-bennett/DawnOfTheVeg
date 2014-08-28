9e86c475d01a2519ad96fd388bcb12521b4a26c9
bennett_richard <rbennett485@gmail.com>
2014-08-28 17:44:04 +0100

    javadoc added and changed to abstract class

--- src/com/rbennett485/dawnoftheveg/Enemy.java
+++ src/com/rbennett485/dawnoftheveg/Enemy.java
@@ -3,21 +3,29 @@ package com.rbennett485.dawnoftheveg;
 import com.badlogic.androidgames.framework.DynamicGameObject;
 import com.badlogic.androidgames.framework.gl.TextureRegion;
 
-public class Enemy extends DynamicGameObject {
+public abstract class Enemy extends DynamicGameObject {
 	
 	public TextureRegion region;
 
 	public Enemy(float x, float y, float width, float height) {
 		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
 	}
 	
+    /**
+     * Returns a new Enemy of the same type, 
+     * with the same position and waypoints
+     * 
+     * @return the new Enemy instance
+     */
     @Override 
-    public Enemy clone() {
-    	return new Enemy(this.position.x, this.position.y, 
-    			this.bounds.width, this.bounds.height); 
-    }
+    public abstract Enemy clone();
 
+    /**
+     * Decreases hp by the projectile's damagePoints.
+     * If hp is below 0 as a result, it is changed to 0
+     * 
+     * @param p The projectile that has hit the enemy
+     */
 	public void hit(Projectile p) {
 		hp -= p.damagePoints;
 		if(hp<0)
