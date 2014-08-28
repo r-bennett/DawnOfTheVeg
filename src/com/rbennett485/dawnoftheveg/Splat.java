2e64740db8ec12a146703e86170706bf16ce5e46
bennett_richard <rbennett485@gmail.com>
2014-08-28 10:49:34 +0100

    initial commit

---
+++ src/com/rbennett485/dawnoftheveg/Splat.java
@@ -0,0 +1,24 @@
+package com.rbennett485.dawnoftheveg;
+
+import com.badlogic.androidgames.framework.GameObject;
+import com.badlogic.androidgames.framework.gl.TextureRegion;
+
+public class Splat extends GameObject {
+	
+	public float stateTime;
+	public static final float TIME_TO_LIVE = 1;
+	public TextureRegion region;
+	
+	public Splat(Enemy e) {
+		super(e.position.x, e.position.y, e.bounds.width, e.bounds.height);
+		
+		if(e instanceof Orange)
+			region = Assets.orangeSplat;
+		else if(e instanceof Grape) 
+			region = Assets.grapeSplat;
+		else if(e instanceof Chilli) 
+			region = Assets.chilliSplat;
+		else
+			region = Assets.peaSplat;
+	}
+}
